6fa45e766a6f32c71c9ae06b39a601225d387a6d
Charles <105826694+FreeCharles02@users.noreply.github.com>
2026-04-02 19:33:13 -0500

    insert
    insert method for bst

--- BST.cpp
+++ BST.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<stack>
+#include <string>
 
 class BST 
 {
@@ -19,10 +20,45 @@ public:
     int getSize() {
         return this->size;
     };
-    void insertOrDelete(TreeNode* newNode) {
+    void insertOrDelete(int value) {
         TreeNode* currNode = this->root;
-        if (this->contains(newNode, currNode) == false)        
+        TreeNode* newNode = new TreeNode(value); 
+            if (this->contains(newNode, currNode) == false) {
+                 this->insert(currNode, newNode);
+               } 
     };
+
+    void insert(TreeNode* newNode, TreeNode* currNode) {
+        if (currNode == nullptr) {
+           this->root = newNode;
+           return;
+           // base case is tree is empty
+        }
+        if (currNode->data > newNode->data) {
+            if (currNode->left == nullptr) {
+                currNode->left = newNode;
+                return;
+            } else {
+                currNode = currNode->left
+                insert(newNode,currNode);
+            }
+        }
+
+        if (currNode->data < newNode->data) {
+            if (currNode->right == nullptr) {
+                currNode->right = newNode;
+                return;
+            } else {
+                currNode = currNode->right;
+                insert(newNode, currNode);
+            }
+        }
+    };
+
+    void _delete(TreeNode* newNode, TreeNode* currNode) {
+        
+    };
+
     bool contains(TreeNode* newNode, TreeNode* currNode) {
        if (currNode == nullptr) {return false;}
        if (currNode->data == newNode->data ) {return true;}
